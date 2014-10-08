9a257b353139658b6aaa528a64338207e2f3f692
Mike Harris <mharris717@gmail.com>
2014-10-08 11:17:31 -0400

    CS

--- app/components/page-numbers.js
+++ app/components/page-numbers.js
@@ -6,8 +6,6 @@ export default Ember.Component.extend({
   totalPages: null,
 
   pageItems: function() {
-    var currentPage, pageNumber, totalPages, _i, _results;
-
     var currentPage = Number(this.get("currentPage"));
     var totalPages = Number(this.get("totalPages"));
     Util.log("PageNumbers#pageItems, currentPage " + currentPage + ", totalPages " + totalPages);
@@ -16,30 +14,30 @@ export default Ember.Component.extend({
     for(var i=1; i<=totalPages; i++) {
       res.push({
         page: i,
-        current: currentPage == 1
+        current: currentPage == i
       });
     }
     return res;
   }.property("currentPage", "totalPages"),
 
   canStepForward: (function() {
-    var page, totalPages;
-    page = Number(this.get("currentPage"));
-    totalPages = Number(this.get("totalPages"));
+    var page = Number(this.get("currentPage"));
+    var totalPages = Number(this.get("totalPages"));
     return page < totalPages;
   }).property("currentPage", "totalPages"),
+
   canStepBackward: (function() {
-    var page;
-    page = Number(this.get("currentPage"));
+    var page = Number(this.get("currentPage"));
     return page > 1;
   }).property("currentPage"),
+
   actions: {
     pageClicked: function(number) {
       Util.log("PageNumbers#pageClicked number " + number);
-      return this.set("currentPage", number);
+      this.set("currentPage", number);
     },
     incrementPage: function(num) {
-      return this.incrementProperty('currentPage', num);
+      this.incrementProperty('currentPage', num);
     }
   }
 });
